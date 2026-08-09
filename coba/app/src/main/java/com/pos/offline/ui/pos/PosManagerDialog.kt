32d8947948a2887b8edb41aa12318587c972d73b
b3bas <benas@gmail.com>
2026-08-09 14:06:16 +0700

    update

--- coba/app/src/main/java/com/pos/offline/ui/pos/PosManagerDialog.kt
+++ coba/app/src/main/java/com/pos/offline/ui/pos/PosManagerDialog.kt
@@ -41,6 +41,7 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
