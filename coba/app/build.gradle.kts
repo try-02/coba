367c3d88583e9af14fd3eb3c417d60dc26495c2e
b3bas <benas@gmail.com>
2026-08-12 17:16:13 +0700

    update

--- coba/app/build.gradle.kts
+++ coba/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.room)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 room {
--- coba/build.gradle.kts
+++ coba/build.gradle.kts
@@ -3,4 +3,15 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.room) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+}
+subprojects {
+    configurations.configureEach {
+        resolutionStrategy.eachDependency {
+            if (requested.group == "org.jetbrains.kotlinx" && requested.name.startsWith("kotlinx-serialization")) {
+                // Paksa menggunakan versi core runtime yang kompatibel dengan Kotlin 2.4.x
+                useVersion("1.11.0") 
+            }
+        }
+    }
 }
