6fbbad8a2eb000f54878259bbc6038e4189589fb
b3bas <benas@gmail.com>
2026-08-15 18:28:53 +0700

    update

--- coba/app/src/androidTest/java/com/pos/offline/GlobalMessageTest.kt
+++ coba/app/src/androidTest/java/com/pos/offline/GlobalMessageTest.kt
@@ -5,7 +5,9 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import androidx.compose.ui.test.ExperimentalTestApi
 
+@OptIn(ExperimentalTestApi::class)
 @RunWith(AndroidJUnit4::class)
 class GlobalMessageTest {
 
