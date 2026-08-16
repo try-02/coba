8c652cf6cf53025efb77b853cf7e730b10128d40
b3bas <benas@gmail.com>
2026-08-16 18:19:11 +0700

    update

--- coba/app/src/main/java/com/pos/offline/ui/inventory/InventoryScreen.kt
+++ coba/app/src/main/java/com/pos/offline/ui/inventory/InventoryScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.imePadding
