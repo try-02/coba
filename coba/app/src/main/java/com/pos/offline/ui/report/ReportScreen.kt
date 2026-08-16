bd473d75428ec667c65f7849b2390dc21c66e3b1
b3bas <benas@gmail.com>
2026-08-16 23:09:03 +0700

    update

--- coba/app/src/main/java/com/pos/offline/ui/report/ReportScreen.kt
+++ coba/app/src/main/java/com/pos/offline/ui/report/ReportScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.LocalOverscrollFactory
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
