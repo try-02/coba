aa3929377ea4d444b34c9e27d59e974d0e044417
b3bas <benas@gmail.com>
2026-08-23 02:02:52 +0700

    update

--- coba2/app/src/main/java/com/pos/offline/data/PosDatabase.kt
+++ coba2/app/src/main/java/com/pos/offline/data/PosDatabase.kt
@@ -1,40 +1,46 @@
+// PosDatabase.kt
 package com.pos.offline.data
 
-import androidx.room3.ColumnTypeConverters
 import androidx.room3.Database
 import androidx.room3.RoomDatabase
-import com.pos.offline.data.converter.DatabaseConverters
-import com.pos.offline.data.dao.ItemTransaksiDao
-import com.pos.offline.data.dao.KasirDao
-import com.pos.offline.data.dao.PembayaranDao
-import com.pos.offline.data.dao.ProdukDao
-import com.pos.offline.data.dao.ReturDao
-import com.pos.offline.data.dao.ShiftDao
-import com.pos.offline.data.dao.TransaksiDao
+import com.pos.offline.data.dao.*
 import com.pos.offline.data.entity.*
 
 @Database(
     entities = [
         ProdukEntity::class,
+        PersediaanEntity::class,
+        PergerakanPersediaanEntity::class,
         KasirEntity::class,
         ShiftEntity::class,
+        PergerakanKasEntity::class,
+        KeranjangEntity::class,
+        ItemKeranjangEntity::class,
         TransaksiEntity::class,
         ItemTransaksiEntity::class,
         PembayaranEntity::class,
         PengembalianEntity::class,
         ItemPengembalianEntity::class,
+        PrinterEntity::class,
+        ProfilTokoEntity::class
     ],
     version = 1,
     exportSchema = true
 )
-@ColumnTypeConverters(DatabaseConverters::class)
+// Tidak perlu anotasi converter sama sekali. Room 3.0 akan menangani Enum secara otomatis.
 abstract class PosDatabase : RoomDatabase() {
-
     abstract fun produkDao(): ProdukDao
+    abstract fun persediaanDao(): PersediaanDao
+    abstract fun pergerakanPersediaanDao(): PergerakanPersediaanDao
     abstract fun kasirDao(): KasirDao
     abstract fun shiftDao(): ShiftDao
+    abstract fun pergerakanKasDao(): PergerakanKasDao
+    abstract fun keranjangDao(): KeranjangDao
+    abstract fun itemKeranjangDao(): ItemKeranjangDao
     abstract fun transaksiDao(): TransaksiDao
     abstract fun itemTransaksiDao(): ItemTransaksiDao
     abstract fun pembayaranDao(): PembayaranDao
     abstract fun returDao(): ReturDao
+    abstract fun printerDao(): PrinterDao
+    abstract fun profilTokoDao(): ProfilTokoDao
 }
