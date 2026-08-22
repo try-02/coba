5bd4bc70102dc19979dae19e44f62958dac04255
b3bas <benas@gmail.com>
2026-08-22 19:55:51 +0700

    update

--- coba2/app/src/main/java/com/pos/offline/data/converter/DatabaseConverters.kt
+++ coba2/app/src/main/java/com/pos/offline/data/converter/DatabaseConverters.kt
@@ -1,23 +1,30 @@
+// DatabaseConverters.kt
 package com.pos.offline.data.converter
-
 import androidx.room3.ColumnTypeConverter
 import com.pos.offline.data.model.*
 
 object DatabaseConverters {
-    @ColumnTypeConverter fun statusKeranjangToString(v: StatusKeranjang) = v.name
-    @ColumnTypeConverter fun stringToStatusKeranjang(v: String) = enumValueOf<StatusKeranjang>(v)
-    @ColumnTypeConverter fun statusTransaksiToString(v: StatusTransaksi) = v.name
-    @ColumnTypeConverter fun stringToStatusTransaksi(v: String) = enumValueOf<StatusTransaksi>(v)
-    @ColumnTypeConverter fun statusShiftToString(v: StatusShift) = v.name
-    @ColumnTypeConverter fun stringToStatusShift(v: String) = enumValueOf<StatusShift>(v)
-    @ColumnTypeConverter fun metodePembayaranToString(v: MetodePembayaran) = v.name
-    @ColumnTypeConverter fun stringToMetodePembayaran(v: String) = enumValueOf<MetodePembayaran>(v)
-    @ColumnTypeConverter fun jenisDiskonToString(v: JenisDiskon) = v.name
-    @ColumnTypeConverter fun stringToJenisDiskon(v: String) = enumValueOf<JenisDiskon>(v)
-    @ColumnTypeConverter fun tujuanStokToString(v: TujuanStokPengembalian) = v.name
-    @ColumnTypeConverter fun stringToTujuanStok(v: String) = enumValueOf<TujuanStokPengembalian>(v)
-    @ColumnTypeConverter fun jenisPersediaanToString(v: JenisPergerakanPersediaan) = v.name
-    @ColumnTypeConverter fun stringToJenisPersediaan(v: String) = enumValueOf<JenisPergerakanPersediaan>(v)
-    @ColumnTypeConverter fun jenisKasToString(v: JenisPergerakanKas) = v.name
-    @ColumnTypeConverter fun stringToJenisKas(v: String) = enumValueOf<JenisPergerakanKas>(v)
+    @ColumnTypeConverter fun statusKeranjangToString(v: StatusKeranjang?): String? = v?.name
+    @ColumnTypeConverter fun stringToStatusKeranjang(v: String?): StatusKeranjang? = v?.let { enumValueOf<StatusKeranjang>(it) }
+
+    @ColumnTypeConverter fun statusTransaksiToString(v: StatusTransaksi?): String? = v?.name
+    @ColumnTypeConverter fun stringToStatusTransaksi(v: String?): StatusTransaksi? = v?.let { enumValueOf<StatusTransaksi>(it) }
+
+    @ColumnTypeConverter fun statusShiftToString(v: StatusShift?): String? = v?.name
+    @ColumnTypeConverter fun stringToStatusShift(v: String?): StatusShift? = v?.let { enumValueOf<StatusShift>(it) }
+
+    @ColumnTypeConverter fun metodePembayaranToString(v: MetodePembayaran?): String? = v?.name
+    @ColumnTypeConverter fun stringToMetodePembayaran(v: String?): MetodePembayaran? = v?.let { enumValueOf<MetodePembayaran>(it) }
+
+    @ColumnTypeConverter fun jenisDiskonToString(v: JenisDiskon?): String? = v?.name
+    @ColumnTypeConverter fun stringToJenisDiskon(v: String?): JenisDiskon? = v?.let { enumValueOf<JenisDiskon>(it) }
+
+    @ColumnTypeConverter fun tujuanStokToString(v: TujuanStokPengembalian?): String? = v?.name
+    @ColumnTypeConverter fun stringToTujuanStok(v: String?): TujuanStokPengembalian? = v?.let { enumValueOf<TujuanStokPengembalian>(it) }
+
+    @ColumnTypeConverter fun jenisPersediaanToString(v: JenisPergerakanPersediaan?): String? = v?.name
+    @ColumnTypeConverter fun stringToJenisPersediaan(v: String?): JenisPergerakanPersediaan? = v?.let { enumValueOf<JenisPergerakanPersediaan>(it) }
+
+    @ColumnTypeConverter fun jenisKasToString(v: JenisPergerakanKas?): String? = v?.name
+    @ColumnTypeConverter fun stringToJenisKas(v: String?): JenisPergerakanKas? = v?.let { enumValueOf<JenisPergerakanKas>(it) }
 }
