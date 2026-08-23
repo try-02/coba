02253f67b8d7a3b902b0465c889ba0dff133eb61
b3bas <benas@gmail.com>
2026-08-23 16:16:50 +0700

    update

--- coba2/app/src/main/java/com/pos/offline/MainActivity.kt
+++ coba2/app/src/main/java/com/pos/offline/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.pos.offline.ui.navigation.PosNavHost
-import com.pos.offline.ui.theme.PosOfflineTheme // Sesuaikan dengan package theme Anda
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- coba2/app/src/main/java/com/pos/offline/PosApplication.kt
+++ coba2/app/src/main/java/com/pos/offline/PosApplication.kt
@@ -1,7 +1,7 @@
 package com.pos.offline
 
 import android.app.Application
-import com.pos.offline.data.di.appModule
+import com.pos.offline.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
